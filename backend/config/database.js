fd17dd2e640b2d978c0d81ddfb3fd2cf3283462b
ayush <ayushacharya999@gmail.com>
2022-09-24 09:39:18 +0545

    working well

---
+++ backend/config/database.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const connectDatabase = () => {
+  mongoose
+    .connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      /** ready to use. The `mongoose.connect()` promise resolves to mongoose instance. */
+      console.log("connected to database");
+    });
+};
+
+module.exports = connectDatabase;
