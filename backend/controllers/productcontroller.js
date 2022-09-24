63a8d0a4bb5eadd818e6efdc7f377289ee298fd9
ayush <ayushacharya999@gmail.com>
2022-09-24 15:50:33 +0545

    idk what i am doing

--- backend/controllers/productcontroller.js
+++ backend/controllers/productcontroller.js
@@ -1,3 +1,18 @@
+//const product = require('../models/product');
+const product =require('../models/product')
+
+//create new products and test in api
+exports.newproduct = async(req,res,next)=>{
+    const product =await product.create(req.body)
+    res.status(201).json({
+        success:true,
+        product
+    })
+}
+
+
+
+
 exports.getproducts=(req,res,next)=>{
     console.log("done");
     res.status(200).json({
@@ -5,3 +20,4 @@ exports.getproducts=(req,res,next)=>{
         message:'this route is for testing purpose only'
     })
 }
+
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,8 +1,15 @@
 const express =require('express')
 const router = express.Router();
-const{getproducts}=require('../controllers/productcontroller')
+
+
+const{getproducts, newproduct}=require('../controllers/productcontroller')
+
+
 router.route('/products').get(getproducts);
+router.route('/product/new').post(newproduct);
+
 
 router.get('/products',getproducts);
+
 module.exports=router;
 
