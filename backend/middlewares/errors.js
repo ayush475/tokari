c1b3249e3879813cc8b08677c87660bf32d5ae2d
ayush <ayushacharya999@gmail.com>
2022-09-29 14:12:22 +0545

    🥱🙄🥳 minor bug fixes

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -3,8 +3,27 @@ const ErrorHandler =require('../utils/errorHandler');
 module.exports =(err,req,res,next)=>{
     err.statusCode = err.statusCode || 500;
     //statuscode 500 => internal server error 
-    err.message= err.message || ' 500 internal server error';
+    //err.message= err.message || ' 500 internal server error';
+    if (process.env.NODE_ENV ==='DEVELOPMENT'){
+        res.status(err.statusCode).json({
+            success:false,
+            error:err,
+            errMessage:err.message,
+            stack :err.stack
+        })
+    }
+    //if we set the process to production
+    if (process.env.NODE_ENV ==='PRODUCTION'){
+        let error ={...err}
+        error.message=err.message
 
+        res.status(err.statusCode).json({
+            success:false,
+            //error:err,
+            message:error.message || "internal server error 500",
+            stack :err.stack
+        })
+    }
     res.status(err.statusCode).json({
         success: false,
         error:err.stack
