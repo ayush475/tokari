e074a5349a07eb853948102d8edfc060b8e96466
ayush <ayushacharya999@gmail.com>
2022-09-29 14:53:25 +0545

    🥱🙄🥳 minor bug fixes

--- backend/controllers/productcontroller.js
+++ backend/controllers/productcontroller.js
@@ -1,16 +1,17 @@
 //const Product = require("../models/product");
 const Product = require("../models/product");
 const ErrorHandler =require('../utils/errorHandler');
+const catchAsyncErrors =require('../middlewares/catchAsyncErrors');
 
 //create new products and test in api
-exports.newproduct = async (req, res, next) => {
+exports.newproduct = catchAsyncErrors( async (req, res, next) => {
   const product = await Product.create(req.body);
 
   res.status(201).json({
     success: true,
     product,
   });
-};
+})
 
 // using try catch
 exports.createnewproductwithtry = async (req, res, next) => {
